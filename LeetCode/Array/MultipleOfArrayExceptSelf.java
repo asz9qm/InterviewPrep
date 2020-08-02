78bfe8a4f1996cd7318e70e27aad9171a9b69408
asz9qm <asz9qm@virginia.edu>
2020-08-02 15:03:17 -0400

    Product except self

---
+++ LeetCode/Array/MultipleOfArrayExceptSelf.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/product-of-array-except-self/
+package LeetCode.Array;
+
+public class MultipleOfArrayExceptSelf 
+{
+    public int[] productExceptSelf(int[] nums) 
+    {
+        int [] left = new int[nums.length];
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++)
+        {
+            left[i] = left[i-1]*nums[i-1];
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) 
+        {
+            left[i] = left[i] * right;
+            right *= nums[i];
+        }
+        return left;
+    }
+    
+}
