d2412bc25abfc78aba92bc510ed732c185bef4c3
asz9qm <asz9qm@virginia.edu>
2020-08-03 19:43:55 -0400

    Number of ones in a binary number

---
+++ LeetCode/Binary/Ones.java
@@ -0,0 +1,16 @@
+package LeetCode.Binary;
+
+public class Ones
+{
+    public int hammingWeight(int n) 
+    {
+        int total = 0;
+        while (n != 0) 
+        {
+            total++;
+            n &= (n - 1);
+        }
+        return total;
+        
+    }
+}
