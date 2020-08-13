fa95e9654c8244b4768f1a88a4b29d0b0b63660b
asz9qm <asz9qm@virginia.edu>
2020-08-12 22:17:05 -0400

    Reverse Linked List

---
+++ LeetCode/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/reverse-linked-list/
+package LeetCode.LinkedList;
+
+public class ReverseLinkedList 
+{
+
+    class ListNode 
+    {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) 
+    {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) 
+        {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+        
+    }
+
+}
