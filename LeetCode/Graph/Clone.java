16a2d7f2bea8b70757faf40da9a04e89a25f77da
asz9qm <asz9qm@virginia.edu>
2020-08-12 22:12:41 -0400

    Clone graph

--- LeetCode/Graph/Clone.java
+++ LeetCode/Graph/Clone.java
@@ -1,10 +1,32 @@
 // https://leetcode.com/problems/clone-graph/
 package LeetCode.Graph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Clone 
 {
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    
     public HashMap<Integer, Node> map = new HashMap<>();
     
     public Node cloneGraph(Node node) 
