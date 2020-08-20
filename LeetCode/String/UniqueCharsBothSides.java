7081af6972bd540ae079292c0395dbdcf0000c5a
asz9qm <asz9qm@virginia.edu>
2020-08-19 22:32:47 -0400

    Unique Characters on both sides

---
+++ LeetCode/String/UniqueCharsBothSides.java
@@ -0,0 +1,59 @@
+public class UniqueCharsBothSides 
+{
+    /**
+     * Returns number of splits in which both sides have
+     * same number of unique characters
+     * @param s the string to split, made of letters a-z
+     * @return the count of the splits
+     */
+    public int unique(String s)
+    {
+        if(s.length() < 2)
+        {
+            return 0;
+        }
+
+        int[] left = new int[26];
+        int[] right = new int[26];
+
+        for(char c : s.toCharArray())
+        {
+            right[c-'a']++;
+        }
+
+        int count = 0;
+        for(char c : s.toCharArray())
+        {
+            left[c-'a']++;
+            right[c-'a']--;
+            if(compare(left, right))
+            {
+                count++;
+            }
+
+        }
+        
+
+    }
+
+    private bool compare(int[] l, int[] r)
+    {
+        for(int i = 0; i < l.length; i++)
+        {
+            if(l[i] > 0 && r[i] > 0)
+            {
+                continue;
+            }
+            else if(l[i] == 0 && r[i] == 0)
+            {
+                continue;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
